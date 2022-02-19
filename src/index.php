73bd7a542547d166876f1799080600b386b6bb75
Daniel Jackson <dajack05@gmail.com>
2022-02-18 21:43:20 -0500

    update

--- src/index.php
+++ src/index.php
@@ -1,6 +1,33 @@
 <?php
 include_once("./DataStore.php");
 
-DataStore::Load("../data/data.json");
+$store = DataStore::Load("../data/data.json");
 
-echo "Hello Worlds" . PHP_EOL;
+function getButtonText(int $position): string
+{
+    return "Boo";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css" />
+    <title>Checklister</title>
+</head>
+
+<body>
+    <div id="checklistHolder">
+        <?php
+        foreach ($store->items as $item) {
+            echo ("<button class=\"check " . ($item->checked ? "checked" : "unchecked") . "\">{$item->label}</button>");
+        }
+        ?>
+    </div>
+</body>
+
+</html>
